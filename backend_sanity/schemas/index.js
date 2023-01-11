f597613a7172e7a16446e5aeb6fec588ea62a302
Decagon <decagon@AG.local>
2023-01-11 14:57:35 +0100

    use of sanity io for backend setup

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,20 @@
+import testimonials from './testimonials'
+import about from './about'
+import brands from './brands'
+import contact from './contact'
+import skills from './skills'
+import experiences from './experiences'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+    testimonials,
+    about,
+    brands,
+    contact,
+    experiences,
+    skills,
+    workExperience,
+    works
+]
+
